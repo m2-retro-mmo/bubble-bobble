410451056a5f7dda8402296f7ca2546d8d763f78
Jonas <fischer-jon@t-online.de>
2022-11-04 10:41:35 +0100

    improved playermovement with ridgid body movement and use collision with hort

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Movement
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+    private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    private ContactFilter2D movementFilter;
+    // speed 
+    public static float baseSpeed = 3f;
+    public float moveSpeed = 3f;
+    public float collisionOffset = 0.1f;
 
-    
+
     private bool holdsDiamond = false;
     private byte teamNumber = 0;
     private bool isCaptured = false;
     // TODO: bubble implementation
 
-    // speed 
-    public static float baseSpeed = 4; 
-    public float speed = 4;
 
     // bubble settings
     private byte maxBubbleCount = 3;
@@ -25,7 +31,7 @@ public class Player : MonoBehaviour
     * checks if the player already holds a diamond, if not the player now holds one
     * @return true successfully collected Diamond, false player already holds a diamond 
     */
-    public bool collectDiamond() 
+    public bool collectDiamond()
     {
         // TODO: change appearance of dragon to dragon holding diamond
         if (holdsDiamond) return false;
@@ -39,12 +45,12 @@ public class Player : MonoBehaviour
         isCaptured = true;
     }
 
-    public void uncapture() 
+    public void uncapture()
     {
         isCaptured = false;
     }
 
-    public void setTeamNumber(byte newTeamNumber) 
+    public void setTeamNumber(byte newTeamNumber)
     {
         teamNumber = newTeamNumber;
     }
@@ -57,7 +63,7 @@ public class Player : MonoBehaviour
     public void shootBubble()
     {
         // TODO: implement cooldown for shooting bubbles here
-        if (bubbleCount > 0) 
+        if (bubbleCount > 0)
         {
             // TODO: spawn bubble and move to direction etc.
         }
@@ -67,15 +73,58 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // get movement input
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 moveInput = new Vector2(
+            moveX,
+            moveY
+        );
+
+        bool success = MovePlayer(moveInput);
+
+        // playermovement was not successful check if player can move in only x or only y direction
+        if (!success)
+        {
+            success = MovePlayer(new Vector2(moveX, 0));
+            if (!success)
+            {
+                success = MovePlayer(new Vector2(0, moveY));
+            }
+        }
+    }
+
+    public bool MovePlayer(Vector2 direction)
+    {
+        int count = rb.Cast(
+            direction,
+            movementFilter,
+            castCollisions,
+            moveSpeed * Time.fixedDeltaTime + collisionOffset
+        );
+
+        if (count == 0)
+        {
+            Vector2 moveVector = direction * moveSpeed * Time.fixedDeltaTime;
+
+            // no collision
+            rb.MovePosition(rb.position + moveVector);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(x, y, 0);
-        transform.Translate(movement * speed * Time.deltaTime);
     }
 }
