937f0c24614a03312bbcca173b7a87dacdb47a01
Jonas <fischer-jon@t-online.de>
2022-11-14 20:28:10 +0100

    added PointCount Prefab

--- Assets/Scripts/Hort.cs
+++ Assets/Scripts/Hort.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Hort : MonoBehaviour
 {
     public int diamonds = 0;
     public byte team = 0;
 
+    public TextMeshProUGUI hortPointCounter;
+
     public void AddDiamond()
     {
         diamonds++;
-        Debug.Log(diamonds);
+        hortPointCounter.text = diamonds.ToString();
     }
 }
