4b709ecfd361f5e3438da3d4d755231ea3a9e205
Sarschar <mahja.sarschar@capgemini.com>
2022-11-14 13:55:14 +0100

    first draft of cellular automata implementation

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 enum EnvironmentType
 {
     Ground,
     Water,
-    Bridge,
-    Obstacle
+    // Bridge,
+    // Obstacle
 }
 
 public class MapCell
@@ -22,45 +23,26 @@ public class MapCell
 
 public class Map : MonoBehaviour
 {
-    private const float tileSize = 4;
-
-    private GameObject root, floor, environment;
-    public GameObject ball;
-    public int xHalfExt = 1;
-    public int zHalfExt = 1;
-
-    public GameObject[] floorTiles;
-
-    private int xExt, zExt;
-    private Vector3 start, end, playfieldOffset;
-
+    [SerializeField] private int width = 200;
+    [SerializeField] private int height = 300;
+    public Tilemap map = GameObject.Find("MapContainer").GetComponent<Tilemap>();
+    // public MapCell[,] cells;
+    // public MapCell[,] gen_cells;
+    public Tile floorTile = Tile.CreateInstance<Tile>();
+    public Tile waterTile = Tile.CreateInstance<Tile>();
     public System.Random ran = new System.Random();
 
-    public MapCell[,] cells;
+    private float noise_density = 60;
+    private int iterations = 10; // low für viele Abschnitt, high für größere Höhlen
 
     void Awake()
     {
-        // root = GameObject.Find("MapContainer");
-        // playfieldOffset = new Vector3(-xHalfExt, 0, -zHalfExt) * tileSize;
-
-        // xExt = 2 * xHalfExt + 1;
-        // zExt = 2 * zHalfExt + 1;
-
-        // var environmentScaleFactor = (float) Mathf.Max(xExt, zExt) / 6f;
-        // environment.transform.localScale = Vector3.one * environmentScaleFactor;
-
-        // var basePlateScaleFactor = (float) Mathf.Max(xExt, zExt) / 3f;
-        // floor.transform.localScale *= basePlateScaleFactor;
-
-        // cells = new MapCell[xExt, zExt];
-
-        // for (int x = 0; x < xExt; x++)
-        // {
-        //     for (int z = 0; z < zExt; z++)
-        //     {
-        //         cells[x, z] = new MapCell();
-        //     }
-        // }
+        // Step 1: Create noise grid --> matches the desired size of the final map
+        // public TileBase[] tiles = { floorTile, waterTile };
+        int[,] grid = generate_noise_grid(noise_density);
+        
+        apply_cellular_automaton(grid, iterations);
+        draw_tilemap(grid, map, floorTile);
     }
 
     // Start is called before the first frame update
@@ -73,5 +55,85 @@ public class Map : MonoBehaviour
     void Update()
     {
         
+    }
+
+    int[,] generate_noise_grid(float density)
+    {
+        int[,] numbers = new int[height, width];
+        int random = ran.Next(1, 100);
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (random > density)
+                {
+                    numbers[i,j] = 0; // tile.floor
+                }
+                else {
+                    numbers[i,j] = 1; // tile.water
+                }
+            }
+        }
+        return numbers;
+    }
+
+    void apply_cellular_automaton(int[,] cells, int iterations)
+    {
+        int i = 0;
+        while(i < iterations)
+        {
+            int[,] tempCells = cells;
+            for (int j = 0; j < height; j++)
+            {
+
+                for (int k = 0; k < width; k++)
+                {
+                    int NeighborWallCount = 0;
+
+                    CheckWalls(j, k);
+                    
+                    if (NeighborWallCount > 4)
+                    {
+                        cells[j,k] = 1; // tile.water
+                    }
+                    else
+                    {
+                        cells[j,k] = 0; // tile.floor
+                    }
+                }
+            }
+            i++;
+        }
+    }
+
+    int CheckWalls(int cellJ, int cellK)
+    {
+        // TODO: Check all neighbors if wall
+        return 4;
+    }
+
+    void draw_tilemap(int[,] map, Tilemap tilemap, TileBase tile)
+    {
+        // var grid = GameObject.Find("MapContainer");
+        tilemap.ClearAllTiles();
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                if (map[x, y] == 0)
+                {
+                    // create tilemap floor
+                    // GetTileByName()
+                    // var mapTile = new GameObject("Tilemap").AddComponent<Tilemap>();
+                    // mapTile.transform.SetParent(map.gameObject);
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                }
+                else {
+                    // create tilemap water
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                }
+            }
+        }
     }
 }
