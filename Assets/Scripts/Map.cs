70e5fd1135ac3066eb77db47e82c303fbcec1111
Mahja Sarschar <Mahja.S@web.de>
2022-11-20 14:05:36 +0100

    create basics for diamond placing on map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 enum EnvironmentType
 {
@@ -66,6 +67,27 @@ public class Map : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Tilemap tilemap = GetComponent<Tilemap>();
+
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+        //int[,] mapCopy = new int[tilemap.,];
+
+        for (int x = 0; x < bounds.size.x; x++) {
+            for (int y = 0; y < bounds.size.y; y++) {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile.name == "sheet_160") {
+                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                    int random = ran.Next(1, 100);
+                    if (random < 40)
+                    {
+                        // TODO: Instantiate Diamond
+                    }
+                } else {
+                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+            }
+        } 
         
     }
 
